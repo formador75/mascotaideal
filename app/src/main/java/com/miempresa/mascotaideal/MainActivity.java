51c9dfc8cacdf4a35e3c1988e57e70efa82114c2
formador75 <misiontic.formador75@uis.edu.co>
2022-11-04 07:55:49 -0500

    Agrego registro usuario a realtimedatabase y firebase

--- app/src/main/java/com/miempresa/mascotaideal/MainActivity.java
+++ app/src/main/java/com/miempresa/mascotaideal/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -21,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
+
                 Intent i = new Intent(MainActivity.this, ListarMascotas.class);
                 startActivity(i);
 
@@ -30,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
         agregarMascota.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(MainActivity.this, AgregarMascota.class);
+                Intent i = new Intent(MainActivity.this, RegisterActivity.class);
                 startActivity(i);
             }
         });
